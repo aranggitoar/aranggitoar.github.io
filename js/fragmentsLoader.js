2805e4af9e34ac8178751c2977489d08b4e952b9
Aranggi Toar <aranggi.josef@gmail.com>
2021-03-29 19:24:47 +0700

    Menambahkan komentar penjelasan di js/fragmentsLoader.js

--- js/fragmentsLoader.js
+++ js/fragmentsLoader.js
@@ -1,9 +1,8 @@
+// Header and footer will be loaded first
 const scriptLoaderPromise = new Promise(function(resolve, reject) {
-
     function loadHtml(parentElementSelector, filePath, runPromise) {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', filePath, true);
-
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 400) {
                 var response = xhr.responseText;
@@ -19,12 +18,12 @@ const scriptLoaderPromise = new Promise(function(resolve, reject) {
         };
         xhr.send();
     }
-
     loadHtml('header', 'header.html', false);
     loadHtml('footer', 'footer.html', true);
-
 });
 
+// Only after the header and footer is loaded
+// the other scripts will be loaded.
 scriptLoaderPromise
     .then(() => {
         let script = document.createElement('script');
@@ -32,6 +31,8 @@ scriptLoaderPromise
         script.type = "text/javascript";
         script.setAttribute("defer", "");
         document.getElementsByTagName('head')[0].appendChild(script);
+        // If the page is the Study Bible page,
+        // add the following script too.
         if (/study/.test(window.location.pathname) === true) {
             let script = document.createElement('script');
             script.src = "js/studyBible.js";
